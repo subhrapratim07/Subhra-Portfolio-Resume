4368c8619475f50912b88cc6f6a8f7c40a9360e5
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2024-08-11 02:04:25 +0530

    Update NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,48 +2,33 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiMenu } from "react-icons/hi";
 import { RxCross2 } from "react-icons/rx";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const navItems = [
-  {
-    id: 0,
-    name: "home",
-  },
-  {
-    id: 1,
-    name: "skills",
-  },
-  {
-    id: 2,
-    name: "works",
-  },
-  {
-    id: 3,
-    name: "resume",
-  },
-  {
-    id: 4,
-    name: "contact",
-  },
+  { id: 0, name: "home" },
+  { id: 1, name: "skills" },
+  { id: 2, name: "works" },
+  { id: 3, name: "resume" },
+  { id: 4, name: "contact" },
 ];
 
 const NavBar = ({ toggleDarkMode, darkMode }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeIndex, setActiveIndex] = useState(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  // Toggle the navbar
   const toggleNav = (name) => {
     setIsOpen(!isOpen);
     setActiveIndex(name === activeIndex ? null : name);
   };
 
-  const [scrollPosition, setScrollPosition] = useState(0);
   useEffect(() => {
+    AOS.init({ duration: 1000 });
     const handleScroll = () => {
       setScrollPosition(window.scrollY);
     };
-
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -51,11 +36,11 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
 
   return (
     <div
-      className={`w-full mx-auto  fixed top-0 py-5 sm:py-4 z-30 ${
-        scrollPosition > 0 ? `bg-white shadow-md` : "bg-transparent"
-      } `}
+      className={`w-full mx-auto fixed top-0 py-5 sm:py-4 z-30 ${
+        scrollPosition > 0 ? "bg-white shadow-md" : "bg-transparent"
+      }`}
     >
-      <nav className=" container m-auto flex items-center justify-between">
+      <nav className="container m-auto flex items-center justify-between">
         <div data-aos="fade-down" className="logo">
           <Link
             onClick={() => window.scrollTo(0, 0)}
@@ -69,26 +54,14 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
           data-aos="fade-down"
           className="nav-items flex items-center space-x-11"
         >
-          {/* hamburger */}
-          <button
-            onClick={toggleNav}
-            className="cursor-pointer text-2xl hidden md:block"
-          >
-            <HiMenu size={25} />
-          </button>
+          
 
           <ul
             className={`flex items-center space-x-11 ${
-              !isOpen ? "md:flex" : "md:right-[0%]"
-            } md:flex-col md:absolute m-auto md:top-0 md:right-[-100%] md:w-[78%] md:h-screen md:bg-white `}
+              isOpen ? "md:flex md:right-0" : "md:hidden"
+            } md:flex-col md:absolute m-auto md:top-0 md:right-[-100%] md:w-[78%] md:h-screen md:bg-white transition-transform duration-300`}
           >
-            {/* Use a button tag for better accessibility */}
-            <button
-              onClick={toggleNav}
-              className={`text-3xl hidden md:block relative right-0 top-4 container mx-auto`}
-            >
-              <RxCross2 size={25} />
-            </button>
+           
             {navItems.map((item) => (
               <li
                 key={item.id}

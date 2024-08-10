240ebcb83de205c669cdd0f15939e4580eb9e6f7
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2024-08-11 02:11:49 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-import resumePDF from "../../assets/Resume.pdf";
+import resumePDF from "../../assets/Subhra.pdf";
 
 
 const Hero = () => {

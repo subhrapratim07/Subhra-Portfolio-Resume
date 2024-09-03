02f597f5910586f7d506ee20d2e7376344e6bd5d
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2024-09-03 16:04:04 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -8,13 +8,7 @@ export const data = [
     link: "https://filehider.netlify.app",
     git: "https://github.com/subhrapratim07/FileHider.git",
   },
-  {
-    id: 2,
-    img: 'https://i.postimg.cc/k59rCvhV/ecommerce-websites.jpg',
-    title: "Ecommerce-Website",
-    link: "https://subhrapratim07.github.io/Ecommerce-Website/",
-    git: "https://github.com/subhrapratim07/Ecommerce-Website",
-  },
+  
   {
     id: 3,
     img: 'https://i.postimg.cc/NfnCjYxW/react-weather-app.png',
@@ -26,7 +20,7 @@ export const data = [
     id: 4,
     img: 'https://i.postimg.cc/8Cv1vS8K/parking.jpg',
     title: "Smart Car Parking System",
-    link: "https://drive.google.com/file/d/1ug3newf6qdGpxI7I0gdYY8o9Imj81xXi/view?usp=drive_link",
+    link: "https://autoparkiq.netlify.app/",
     git: "https://github.com/subhrapratim07/CarParkingAutoParkIQ",
   }
   

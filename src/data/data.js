bc2c51efc8871c50956efda95d6dc4b21c1febaf
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2025-04-28 00:34:27 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -11,26 +11,15 @@ export const data = [
   
   {
     id: 3,
-<<<<<<< HEAD
     img: 'https://i.postimg.cc/fkQ9yfKq/event.jpg',
     title: "Event Booking App",
     link: "https://festivifyappspm.netlify.app/",
     git: "https://github.com/subhrapratim07/Festivify",
-=======
-    img: 'https://i.postimg.cc/NfnCjYxW/react-weather-app.png',
-    title: "Weather-App",
-    link: "https://weather-app-n5n8.onrender.com",
-    git: "https://github.com/subhrapratim07/Weather-App",
->>>>>>> bc5de83513045831c6612a0300fb4636665bfd33
   },
   {
     id: 4,
     img: 'https://i.postimg.cc/8Cv1vS8K/parking.jpg',
-<<<<<<< HEAD
     title: "Smart Car Parking System App",
-=======
-    title: "Smart Car Parking System",
->>>>>>> bc5de83513045831c6612a0300fb4636665bfd33
     link: "https://autoparkiq.netlify.app/",
     git: "https://github.com/subhrapratim07/CarParkingAutoParkIQ",
   }

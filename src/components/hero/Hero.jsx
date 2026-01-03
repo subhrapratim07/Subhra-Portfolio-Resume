9c8d71c92802be16cccc2c4c110145978568343d
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2026-01-04 01:00:48 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -106,7 +106,7 @@ const Hero = () => {
             <img
             data-aos="fade-up"
               className=" h-[90%]  w-full object-cover md:h-[95%] md:m-auto sm:m-0"
-              img src='htthttps://i.ibb.co/4CTBN6y/Gemini-Generated-Image-qdme4nqdme4nqdme-removebg-preview.png'
+              img src='https://i.ibb.co/4CTBN6y/Gemini-Generated-Image-qdme4nqdme4nqdme-removebg-preview.png'
               // src={mine}
               alt="mine"
             />
